150aecdb5b3b3a80458036bbcec79144e59d4892
Dennis Gahm (Note 20) <dennisgahm@gmail.com>
2026-09-07 18:25:23 +0900

    supercomputer

--- phd.java
+++ phd.java
@@ -1,9 +1,31 @@
 public class phd
 {
     public static void main(String[] args)
-    {}
+    {
+	Computer superComputer = new Computer();
+    }
 }
 
 class Computer
 {
+    long[] computerIDs = new long[1000];
+
+    public Computer()
+    {
+	computerIDs[0] = 555;
+	System.out.println("alpha particle 555 go");
+
+	long number = 384927;
+
+	for (int i=0;i<1000;i++)
+	    {
+		computerIDs[i]= ((int) (number/50)) + ((int)(number /253));
+		if ((((int) computerIDs[i]) % 2)==0)
+		    {
+			computerIDs[i] =computerIDs[i] - number / (500 -2);
+		    }
+
+		System.out.println("ID"+i+ " " +Long.toString(computerIDs[i]));
+	    }
+    }
 }
